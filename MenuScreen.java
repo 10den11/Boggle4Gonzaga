6c69220640272b425469707e6277ec03a5970e24
10den11 <domgianatassio@gmail.com>
2017-04-10 13:41:36 -0700

    Finished Main Menu
    updated the main menu so everything is in the right place

--- MenuScreen.java
+++ MenuScreen.java
@@ -9,6 +9,7 @@ public class MenuScreen {
 				frame.setTitle("Yahtzee");
 				frame.setLocation(0, 0);
 				frame.setVisible(true);
+				frame.setBackground(Color.BLACK);
 			}
 		});
 
@@ -17,19 +18,25 @@ public class MenuScreen {
 }
 class MenuFrame extends JFrame {
 	private JPanel menuScreen;
+	private Box box;
 	
 	public MenuFrame() {
 		setSize(1920, 1080);
+		menuScreen = new JPanel();
 		JButton PlayButton = new JButton();
 		JButton OptionButton = new JButton();
 		JButton HighScoreButton = new JButton();
-		menuScreen = new JPanel();
-		//menuScreen.setLayout(new GridLayout(0, 1, 100, 100));
+		ImageIcon TitleIcon = new ImageIcon("title.png");
+		JLabel titleLabel = new JLabel ("", TitleIcon, JLabel.CENTER);
+		box = Box.createVerticalBox();
+		box.setOpaque(true);
+		box.setBackground(Color.BLACK);
 		menuScreen.setBackground(Color.BLACK);
 		PlayButton.setIcon(new ImageIcon("start.png"));
 		OptionButton.setIcon(new ImageIcon("option.png"));
 		HighScoreButton.setIcon(new ImageIcon("highscore.png"));
-		/*PlayButton.setOpaque(false);
+		
+		PlayButton.setOpaque(false);
 		OptionButton.setOpaque(false);
 		HighScoreButton.setOpaque(false);
 		PlayButton.setContentAreaFilled(false);
@@ -37,14 +44,28 @@ class MenuFrame extends JFrame {
 		HighScoreButton.setContentAreaFilled(false);
 		PlayButton.setBorderPainted(false);
 		OptionButton.setBorderPainted(false);
-		HighScoreButton.setBorderPainted(false);*/
+		HighScoreButton.setBorderPainted(false);
+		
+		PlayButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		OptionButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		HighScoreButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
 		PlayButton.setPreferredSize(new Dimension(735, 135));
 		OptionButton.setPreferredSize(new Dimension(1033, 133));
 		HighScoreButton.setPreferredSize(new Dimension(1338, 134));
 		
-		menuScreen.add(PlayButton);
+		/*menuScreen.add(PlayButton);
 		menuScreen.add(OptionButton);
 		menuScreen.add(HighScoreButton);
-		add(menuScreen, BorderLayout.CENTER);
+		add(menuScreen, BorderLayout.CENTER);*/
+		
+		box.add(PlayButton);
+		box.add(Box.createVerticalStrut(100));
+		box.add(OptionButton);
+		box.add(Box.createVerticalStrut(100));
+		box.add(HighScoreButton);
+		menuScreen.add(titleLabel);
+		add(box, BorderLayout.CENTER);
+		add(menuScreen, BorderLayout.NORTH);
 	}
 }
